0e71dec1c6991d29923e348e85932d67f86528a4
Casu <gacasu@gmail.com>
2024-02-06 20:47:35 -0300

    Add cinema details endpoint

--- src/api/routes.js
+++ src/api/routes.js
@@ -2,12 +2,14 @@ const express = require('express')
 const {
   createCinema,
   getCinemas,
+  getCinema,
 } = require('./handlers/cinema')
 
 const router = express.Router()
 
 // Define all API routes here
 router.get('/cinemas', getCinemas)
+router.get('/cinemas/:id', getCinema)
 router.post('/cinemas', createCinema)
 
 module.exports = { router }
