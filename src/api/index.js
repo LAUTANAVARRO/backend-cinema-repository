f7732721a8508da201bde83590df1a2ca96ec276
Casu <gacasu@gmail.com>
2024-03-01 22:47:44 -0300

    add cors

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const { router } = require('./routes')
 
 const port = process.env.PORT || 7001
 
+// middlewares
+app.use(cors())
 app.use(router)
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}...`)
 })
